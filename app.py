7e571e9a753db2482adcc63929d8c6dcbbf24fb5
Baptiste MOINE <contact@bmoine.fr>
2026-02-15 15:09:42 +0100

    Add Docker support and bind to 0.0.0.0

--- app.py
+++ app.py
@@ -348,4 +348,4 @@ if __name__ == '__main__':
     if not os.path.exists(DATABASE):
         print("Initializing database...")
         init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
